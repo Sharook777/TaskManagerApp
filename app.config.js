105dba68ddbd643e05e23efb0dbe3d996da5184b
Sharook <sharookcs@gmail.com>
2025-03-03 19:21:45 +0530

    config

--- app.config.js
+++ app.config.js
@@ -4,6 +4,13 @@ export default {
   expo: {
     extra: {
       API_URL: process.env.API_URL,
+      eas: {
+        projectId: process.env.EAS_PROJECT_ID,
+      },
+    },
+    owner: "siak_7",
+    android: {
+      package: "com.siak_7.taskmanager",
     },
   },
 };
